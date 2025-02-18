c02dfaebe1cad758b4402c57f4dc13c5d42acde3
mo96132960 <mohaki.eg@gmail.com>
2025-02-18 23:20:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function openPorshor(language) {
     const content = document.getElementById('porshor-content');
     
     if (language === 'english') {
-        const imageUrl = `/Mohaki_Brochure_English.png`; // Adjust this path according to where your images are stored
+        const imageUrl = `bit.ly/Moh_Eng_Brochure`; // Adjust this path according to where your images are stored
     window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     } else if (language === 'arabic') {
-        const imageUrl = `/Mohaki_Brochure_Arabic.png`; // Adjust this path according to where your images are stored
+        const imageUrl = `bit.ly/Moh_Arab_Brochure`; // Adjust this path according to where your images are stored
     window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     }
