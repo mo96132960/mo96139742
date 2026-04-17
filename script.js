f199b5299121822ac9f622676dcb0a0b96d250f0
mo96132960 <mohaki.eg@gmail.com>
2026-04-18 01:53:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function openPorshor(language) {
     const content = document.getElementById('porshor-content');
     
     if (language === 'english') {
-        const imageUrl = `/`; // Adjust this path according to where your images are stored
+        const imageUrl = `#`; // Adjust this path according to where your images are stored
     window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     } else if (language === 'arabic') {
-        const imageUrl = `/`; // Adjust this path according to where your images are stored
+        const imageUrl = `#`; // Adjust this path according to where your images are stored
     window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     }
