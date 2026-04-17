87b4b9ce82f476309656f99958e94e4b06a59c13
mo96132960 <mohaki.eg@gmail.com>
2026-04-18 01:54:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function openPorshor(language) {
     
     if (language === 'english') {
         const imageUrl = `#`; // Adjust this path according to where your images are stored
-    window.open(imageUrl, '_blank'); // This will open the image in a new tab
+    //window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     } else if (language === 'arabic') {
         const imageUrl = `#`; // Adjust this path according to where your images are stored
-    window.open(imageUrl, '_blank'); // This will open the image in a new tab
+    //window.open(imageUrl, '_blank'); // This will open the image in a new tab
 ;
     }
 }
